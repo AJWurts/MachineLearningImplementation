e0d81626226b183d959d63e2f14ba6ac9d1ef6ab
Alex Wurts <ajwurts@gmail.com>
2018-10-14 15:45:01 -0400

    Updated some stuff

--- kmeans.py
+++ kmeans.py
@@ -2,22 +2,41 @@ import random
 from visualization import plot_points
 
 class Point:
-  def __init__(self, x, y, id=None, cluster=None):
-    self.x = x
-    self.y = y
+  def __init__(self, attrs, id=None, cluster=None):
+    self.attrs = attrs
     if id is not None:
       self.id = id
     self.id = -1
     self.cluster = None
 
+  def __getitem__(self, i):
+    return self.attrs[i]
+
+  def __getattr__(self, attr):
+    if attr == 'x':
+      return self.attrs[0]
+    elif attr == 'y':
+      return self.attrs[1]
+    elif attr == 'attrs':
+      return self.attrs
+    elif attr == 'id':
+      return self.id
+    elif attr == 'cluster':
+      return self.cluster
+
   def dist_to(self, other):
-    return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+    the_sum = 0
+    for i in range(len(self.attrs)):
+      if (type(self.attrs[i]) is int or type(self.attrs[i]) is float)\
+          and (type(other.attrs[i] is int or type(other.attrs[i] is float))):
+        the_sum += (self.attrs[i] - other.attrs[i]) ** 2
+    return the_sum ** 0.5
 
   def setCluster(self, cluster):
     self.cluster = cluster.id
 
   def __repr__(self):
-    return "[{0}, {1}]".format(self.x, self.y)
+    return str(self.attrs)
 
   @staticmethod
   def _parseNum(num):
@@ -29,26 +48,23 @@ class Point:
     x = Point._parseNum(coord[0])
     y = Point._parseNum(coord[1])
 
-    return x, y
+    return [x, y]
   
-  def min(self):
-    if self.x < self.y:
-      return self.x
-    else:
-      return self.y
-
-  def max(self):
-    if self.x > self.y:
-      return self.x
-    else:
-      return self.y
-
   def __eq__(self, other):
-    return self.x == other.x and self.y == other.y
+    if len(self.attrs) != len(other.attrs):
+      return False
+    
+    for a in range(len(self.attrs)):
+      if (type(self.attrs[i]) is int or type(self.attrs[i]) is float)\
+          and (type(other.attrs[i] is int or type(other.attrs[i] is float))):
+          if self.attrs[i] != other.attrs[i]:
+            return False
+
+    return True
 
   
   def __ne__(self, other):
-    return self.x != other.x or self.y != other.y
+    return not self == other
 
 
 class Cluster:
@@ -63,13 +79,15 @@ class Cluster:
   def reCalcCenter(self):
     if len(self.points) == 0:
       return
-    x_sum = 0
-    y_sum = 0
+    var_sum = [0 for _ in range(len(self.center.args))]
+
     for p in self.points:
-      x_sum += p.x
-      y_sum += p.y
+      for i in range(len(p)):
+        var_sum[i] += p[i]
     
-    self.center = Point(x_sum / len(self.points), y_sum / len(self.points))
+    new_attrs = [var_sum[i] / len(self.points) for i in range(len(self.center.attrs))]
+
+    self.center = Point(new_attrs)
   
   def clear(self):
     self.points.clear()
@@ -88,6 +106,23 @@ class Cluster:
     return str(self.center)
 
 
+def getMinMax(points):
+  minMax = [[None, None] for _ in range(len(points[0].attrs))]
+
+  for p in points:
+    for i, a in enumerate(p.attrs):
+      if minMax[i][0] is not None and minMax[i][0] > a:
+        minMax[i][0] = a
+      else:
+        minMax[i][0] = a
+
+      if minMax[i][1] is not None and minMax[i][1] < a:
+        minMax[i][0] = a
+      else:
+        minMax[i][1] = a
+      
+  return minMax
+
 
 
 def getPointsFromFile(file):
@@ -98,23 +133,21 @@ def getPointsFromFile(file):
     for line in split:
       if line != '':
         point_split = line.split('\t')
-        point = Point(*Point.parseCoord(point_split[1:]), int(point_split[0]))
+        point = Point(Point.parseCoord(point_split[1:]), int(point_split[0]))
         points.append(point)
     return points
 
 
 def kmeans(points, num_clusters, plot=False):
-  minX = min([p.x for p in points])
-  maxX = max([p.x for p in points])
-  minY = min([p.y for p in points])
-  maxY = max([p.y for p in points])
-  print(minX, maxY, minY, maxY)
+  minMax = getMinMax(points)
   clusters = []
 
   for i in range(num_clusters):
-    x = random.uniform(minX, maxX)
-    y = random.uniform(minY, maxY)
-    clusters.append(Cluster(Point(x, y), i))
+    attrs = []
+    for key in range(len(points[0].attrs)):
+      attrs[key] = random.uniform(minMax[key][0], minMax[key][1])
+
+    clusters.append(Cluster(Point(attrs), i))
 
   while True:
     for p in points:
@@ -163,25 +196,25 @@ def kmeans(points, num_clusters, plot=False):
   return clusters
 
   
+if __name__ == "__main__":
+
+  points = getPointsFromFile('cluster_data.txt')
+  minX = min([p.x for p in points])
+  maxX = max([p.x for p in points])
+  minY = min([p.y for p in points])
+  maxY = max([p.y for p in points])
+  pRange = [minX, maxX, minY, maxY]
+  clusters = kmeans(points, 6)
+
+  img, _ = plot_points([Point([-100 for _ in points[0].attrs])], fill='black')
+  img.show()
+  colors = ['red', 'blue', 'white', 'purple', 'orange', 'navy']
+  for i, c in enumerate(clusters):
+    img, _ = plot_points(c.points, fill=colors[i], image=img, pRange=pRange, axis=True)
+    img, _ = plot_points([c.center], fill='green', image=img, pRange=pRange, label='Cluster ' + str(i), axis=True)
 
 
-points = getPointsFromFile('cluster_data.txt')
-minX = min([p.x for p in points])
-maxX = max([p.x for p in points])
-minY = min([p.y for p in points])
-maxY = max([p.y for p in points])
-pRange = [minX, maxX, minY, maxY]
-clusters = kmeans(points, 6)
+  img.show()
 
-img, _ = plot_points([Point(-100, -100)], fill='black')
-img.show()
-colors = ['red', 'blue', 'white', 'purple', 'orange', 'navy']
-for i, c in enumerate(clusters):
-  img, _ = plot_points(c.points, fill=colors[i], image=img, pRange=pRange, axis=True)
-  img, _ = plot_points([c.center], fill='green', image=img, pRange=pRange, label='Cluster ' + str(i), axis=True)
-
-
-img.show()
-
-print(clusters)
+  print(clusters)
 
